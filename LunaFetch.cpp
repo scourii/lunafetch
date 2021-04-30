8b2f1156f786abac57a9c72917c6f97e592cdfa7
SugarBlank <eneblank19@protonmail.com>
2021-04-30 17:50:40 -0400

    removed redundant imports, added color, more

--- LunaFetch.cpp
+++ LunaFetch.cpp
@@ -1,18 +1,16 @@
 #include <stdlib.h>
-#include <time.h>
 #include <sys/utsname.h>
 #include <iostream>
 #include <pwd.h>
-#include <vector>
-#include <cstdlib>
-#include <pwd.h>
 #include <cmath>
 #include <unistd.h>
-#include <stdbool.h>
 #include <string.h>
 #include <fstream>
-#include <chrono>
-#include <utmp.h>
+
+// Terminal Colors
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
 
 struct dist {
 	char *col1, *col2, *col3, *col4, *col5, *col6, *col7, *col8, *col9, *col10, *col11;
@@ -25,8 +23,8 @@ std::string info[] = {
 "            ~+",
 "",
 "                 *       +",
-"           '                  |",
-"       ()    .-.,='``'=.    - o -",
+"           '                  |", 
+"       ()    .-.,='``'=.    - o -", 
 "             '=/_       \\     |",
 "          *   |  '=._    |",
 "               \\     `=./`,        '",
@@ -52,7 +50,7 @@ void printout(std::string array[], int size)
 static std::string *name()
 {
     username = cuserid(username);
-    std::cout << "User:     " << username << std::endl;
+    std::cout << CYAN << "User:     " <<  WHITE << username << std::endl;
     return 0;
 }
 
@@ -63,7 +61,14 @@ static std::string *shell()
 	if (slash) {
 		shellname = slash + 1;
 	}
-    std::cout << "Shell:    " << shellname << std::endl;
+    std::cout << CYAN << "Shell:    " << WHITE << shellname << std::endl;
+    return 0;
+}
+static std::string *editor()
+{
+    const char* editor = std::getenv("EDITOR");
+    if (editor && *editor) editor = basename(editor);
+        std::cout << CYAN << "Editor:   " << WHITE << editor << std::endl;
     return 0;
 }
 
@@ -71,14 +76,14 @@ static std::string *get_os()
 {
     struct utsname uname_info;
     if(uname(&uname_info)) exit(-1);
-    std::cout << "OS:       " << uname_info.sysname << " " << uname_info.machine << std::endl;
+    std::cout << CYAN << "OS:       " << WHITE << uname_info.sysname << " " << uname_info.machine << std::endl;
     return 0;
 }
 static std::string *kernel()
 {
     struct utsname uname_info;
     if(uname(&uname_info)) exit(-1);
-    std::cout << "Kernel:   " << uname_info.release << std::endl;
+    std::cout << CYAN << "Kernel:   " << WHITE << uname_info.release << std::endl;
     return 0;
 }
 static void uptime()
@@ -90,7 +95,12 @@ static void uptime()
     double uptime;
     fscanf(file, "%lf", &uptime);
     fclose(file);
-    std::cout << "Uptime:   " << round(uptime / 60) << " mins" << std::endl;
+    uptimeHour = uptime / 3600;
+    uptimeMin = round(uptime / 180);
+    if (uptimeHour >  1)
+        std::cout << CYAN << "Uptime:   " << WHITE << uptimeMin << " mins" << std::endl;
+    else
+        std::cout << CYAN << "Uptime:   " << WHITE << uptimeHour << " hour " << uptimeMin<< " mins" << std::endl;
 }
 
 int main() {
@@ -99,6 +109,7 @@ int main() {
     name();
     get_os();
     kernel();
+    editor();
     shell();
     uptime();
     return 0;
